0803bb2c41da517803527a444da46acb64a14149
Brian Huynh <brianhuynh31@gmail.com>
2023-08-12 00:18:20 -0700

    update routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import Home from './components/Home'
@@ -6,14 +7,22 @@ import Food from './components/Food';
 import Quote from './components/Quote';
 
 function App() {
+
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  const handleGetStartedClick = () => setShowNavbar(true);
+
   return (
     <div>
-      <Navbar/>
-      <Home/>
+      {showNavbar && <Navbar />}
       <Routes>
-        <Route path="/goal" element={<Goal/>} />
-        <Route path="/food" element={<Food/>} />
-        <Route path="/quote" element={<Quote/>} />
+        <Route
+          path="/"
+          element={<Home handleGetStartedClick={handleGetStartedClick} />}
+        />
+        <Route path="/goal" element={<Goal />} />
+        <Route path="/food" element={<Food />} />
+        <Route path="/quote" element={<Quote />} />
       </Routes>
     </div>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Home = () => {
+const Home = ({ handleGetStartedClick }) => {
     return (
-        <div className='w-full h-screen justify-center items-center flex flex-col'>
-                <div className='max-w-[1000px]'>
-                    <div className='text-container flex flex-col items-center overflow-hidden h-60'>
+        <div className='h-screen justify-center items-center flex flex-col'>
+                <div className='max-w-[1000px] text-center'>
                         <div className="animate-fade-up animate-ease-in">
                             
                             <p className='text-3xl'>Do you ever feel like your days are like a routine?</p>
                     
                         </div>
-                        <div className="animate-fade-up animate-ease-in animate-delay-1000">
+                        <div className="animate-fade-up animate-ease-in animate-delay-1000 py-4">
                             
                             <h1 className='text-3xl'> Let's spice things up!</h1>
                     
                         </div>
                         <div className="animate-fade-up animate-ease-in animate-delay-[2000ms]">
                             
-                            <p className='text-3xl'>Try a new goal to achieve, a food to try, or a quote to live by once a day.</p>
+                            <p className='text-3xl'>Achieve a new goal, a food to try, or a quote to live by once a day.</p>
                     
                         </div>
-                    </div>
+
+                        <div>
+                            <button onClick={handleGetStartedClick} className='animate-fade-up animate-ease-in animate-delay-[3000ms] rounded-md border-2 border-black px-6 py-3 my-8'>
+                                <Link to='/goal'>
+                                    Get Started
+                                </Link>
+                            </button>
+                        </div>
+                    
                 </div>
 
 
